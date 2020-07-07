2cdf4cabf3b48dc1daa908fb2108bdac172136d2
Diones Alves <pereiradiones987@gmail.com>
2020-07-07 14:02:43 -0300

    Iniciando o projeto e criando a tabela com os usuários

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Controle de Usuários</title>
+</head>
+<body>
+    <table border="1" width="100%">
+        <th>Nome</th>
+        <th>E-mail</th>
+        <th>Ações</th>
+    </table>
+
+    <style>
+        table{
+            border-collapse: collapse;
+        }
+    </style>
+</body>
+</html>
